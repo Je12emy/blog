dbb1fdff48f2051a8b24924b087eeabdd498e5d6
Jeremy <jeremy@je12emy.com>
2022-09-19 13:05:30 -0600

    feat: add solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), solidJs()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
