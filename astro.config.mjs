6c5d5ccf6d6927d94652609af1d7f0dac8d68bfd
Jeremy <jeremy@je12emy.com>
2023-03-25 14:09:07 -0600

    feat: update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://je12emy.com/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
