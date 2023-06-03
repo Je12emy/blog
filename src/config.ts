2a25459c06f6f5c1eaab4f4def0eb623f4dec43f
Jeremy <jeremy@je12emy.com>
2023-06-03 03:02:33 -0600

    feat: add support for rss

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = `Jeremy's Blog`;
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE = "https://www.je12emy.com"
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,22 @@
 import rss from '@astrojs/rss';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
+import { SITE_TITLE, SITE_DESCRIPTION, SITE } from '../config';
+import { getCollection } from "astro:content";
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
-export const get = () =>
-	rss({
+export const get = async () => {
+	const blog = await getCollection('blog');
+	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
-		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
-	});
+		site: SITE,
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/blog/${post.slug}/`,
+			content: sanitizeHtml(parser.render(post.body)),
+		})),
+	})
+}
